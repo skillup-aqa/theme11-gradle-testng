afd751ed11adc96e28a06ba9be28a89e6afab21d
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2024-05-01 20:30:11 +0200

    Adding dummy test and modifying build.gradle

---
+++ src/test/java/ua/skillup/IntArrayOperationsTest.java
@@ -0,0 +1,12 @@
+package ua.skillup;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class IntArrayOperationsTest {
+    @Test
+    public void dummyTest() {
+        assertTrue(false);
+    }
+}
